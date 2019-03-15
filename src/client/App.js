19b297e35e257150603d707770c2c20456fcf342
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2019-03-15 15:26:14 +0000

    can add a new post however it is not saved

--- src/client/App.js
+++ src/client/App.js
@@ -19,10 +19,32 @@ const posts = [{
 }];
 
 export default class App extends Component {
+    
     state = {
         posts: posts,
         postContent: ''
     }
+
+    handlePostContentChange = (event) => {
+        this.setState({postContent: event.target.value})
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const newPost = {
+          id: this.state.posts.length + 1,
+          text: this.state.postContent,
+          user: {
+            avatar: '/uploads/avatar1.png',
+            username: 'Fake User'
+          }
+        };
+        this.setState((prevState) => ({
+          posts: [newPost, ...prevState.posts],
+          postContent: ''
+        }));
+    }
+    
     render() {
         const {posts, postContent} = this.state
 
