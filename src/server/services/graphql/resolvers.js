80775ed8cdcadfb73e8c278bac2edfdacead3e33
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2019-03-15 22:26:10 +0000

    still setting up the relationships but it looks like it is working

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -20,7 +20,12 @@ export default function resolver() {
                 logger.log({ level: 'info', message: 'Post was created' });
                 return postObject;
             },
-        }
+        },
+        Post: {
+            user(post, args, context) {
+              return post.getUser();
+            },
+          },
     }
     return resolvers;
 }
