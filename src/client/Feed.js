77e0ef6dd04d255f4d134cd952dcc04d63464144
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2019-03-16 21:07:52 +0000

    this refactor is not looking great at the moment, bit broken but will keep going

--- src/client/Feed.js
+++ src/client/Feed.js
@@ -1,58 +1,41 @@
-import React, { Component } from 'react'
-import '../../assets/css/style.css';
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql, compose } from 'react-apollo';
 
-export default class Feed extends Component {
-
-    state = {
-        posts: posts,
-        postContent: ''
+const GET_POSTS = gql`{ 
+  posts { 
+    id
+    text
+    user {
+      avatar
+      username
     }
+  }
+}`;
 
-    handlePostContentChange = (event) => {
-        this.setState({ postContent: event.target.value })
+const ADD_POST = gql`
+  mutation addPost($post : PostInput!) {
+    addPost(post : $post) {
+      id
+      text
+      user {
+        username
+        avatar
+      }
     }
+  }
+`;
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const newPost = {
-            id: this.state.posts.length + 1,
-            text: this.state.postContent,
-            user: {
-                avatar: '/uploads/avatar1.png',
-                username: 'SkinnyPigeon'
-            }
-        };
-        this.setState((prevState) => ({
-            posts: [newPost, ...prevState.posts],
-            postContent: ''
-        }));
-    }
-
-    render() {
-        const { posts, postContent } = this.state
-
-        return (
-            <div className="container">
-                <div className="postForm">
-                    <form onSubmit={this.handleSubmit}>
-                        <textarea value={postContent} onChange={this.handlePostContentChange} placeholder="Write your custom post!" />
-                        <input type="submit" value="Submit" />
-                    </form>
-                </div>
-                <div className="feed">
-                    {posts.map((post, i) =>
-                        <div key={post.id} className="post">
-                            <div className="header">
-                                <img src={post.user.avatar} />
-                                <h2>{post.user.username}</h2>
-                            </div>
-                            <p className="content">
-                                {post.text}
-                            </p>
-                        </div>
-                    )}
-                </div>
-            </div>
-        )
-    }
-}
+const ADD_POST_MUTATION = graphql(ADD_POST, {
+    name: 'addPost'
+  });
+  
+  const GET_POSTS_QUERY = graphql(GET_POSTS, {
+    props: ({ data: { loading, error, posts } }) => ({
+      loading,
+      posts,
+      error
+    })
+  });
+  
+  export default compose(GET_POSTS_QUERY, ADD_POST_MUTATION)(Feed);
