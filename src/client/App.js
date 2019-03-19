0ee7ff69f3fef920aaa5046f5f7c99b33961daa0
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2019-03-19 22:27:12 +0000

    think it's coming along ok

--- src/client/App.js
+++ src/client/App.js
@@ -5,8 +5,21 @@ import Chats from './Chats';
 import '../../assets/css/style.css';
 import './components/fontawesome';
 import Bar from './components/bar';
+import LoginRegisterForm from './components/loginregister';
 
 export default class App extends Component {
+    state = {
+        loggedIn: false
+    }
+    componentWillMount() {
+        const token = localStorage.getItem('jwt');
+        if (token) {
+            this.setState({ loggedIn: true });
+        }
+    }
+    changeLoginState = (loggedIn) => {
+        this.setState({ loggedIn });
+    }
     render() {
         return (
             <div className="container">
@@ -14,6 +27,7 @@ export default class App extends Component {
                     <title>Graphbook - Feed</title>
                     <meta name="description" content="Newsfeed of all your friends on Graphbook" />
                 </Helmet>
+                <LoginRegisterForm changeLoginState={this.changeLoginState}/>
                 <Bar />
                 <Feed />
                 <Chats />
