dff8c3060abfbe9f5e21da93812150e7bec72aad
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2019-03-21 22:27:23 +0000

    can see the user profile now

--- src/client/components/bar/index.js
+++ src/client/components/bar/index.js
@@ -3,19 +3,23 @@ import SearchBar from './search';
 import UserBar from './user';
 import { UserConsumer } from '../context/user';
 import Logout from './logout';
+import Home from './home';
 
 export default class Bar extends Component {
     render() {
         return (
             <div className="topbar">
                 <div className="inner">
-                    <SearchBar/>
+                    <SearchBar />
                     <UserConsumer>
                         <UserBar />
                     </UserConsumer>
                 </div>
                 <div className="buttons">
-                    <Logout />
+                    <div className="buttons">
+                        <Home />
+                        <Logout changeLoginState={this.props.changeLoginState} />
+                    </div>
                 </div>
             </div>
         );
--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -74,7 +74,7 @@ export default function resolver() {
                     }],
                 });
             },
-            postsFeed(root, { page, limit }, context) {
+            postsFeed(root, { page, limit, username }, context) {
                 var skip = 0;
               
                 if(page && limit) {
@@ -89,10 +89,21 @@ export default function resolver() {
                 if(limit) {
                     query.limit = limit;
                 }
-              
+
+                if(typeof username !== typeof undefined) {
+                    query.include = [{model: User}];
+                    query.where = { '$User.username$': username };
+                }
                 return {
                     posts: Post.findAll(query)
                 };
+            },
+            user(root, { username }, context) {
+                return User.findOne({
+                  where: {
+                    username: username
+                  }
+                });
             },
             usersSearch(root, { page, limit, text }, context) {
                 if(text.length < 3) {
